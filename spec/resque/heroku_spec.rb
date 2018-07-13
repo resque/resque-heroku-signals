69c8749e9ffccffd013cbcd0f0b6238bdaa32d6c
Michael Bianco <iloveitaly@gmail.com>
2018-07-13 07:48:37 -0600

    Test for heroku_will_terminate?

--- spec/resque/heroku_spec.rb
+++ spec/resque/heroku_spec.rb
@@ -8,6 +8,32 @@ class DummyJob
   end
 end
 
+def LongCleanupJob
+  def self.perform(uuid)
+    sleep 2
+
+    if Resque.heroku_will_terminate?
+      FileUtils.touch(uuid)
+    end
+  end
+end
+
+def get_job_pid(worker)
+  pid = nil
+  wait condition: -> () {
+    pid = worker.instance_variable_get("@child")
+    pid.nil?
+  }
+  pid
+end
+
+def wait(timeout: 5000, condition:)
+  t0 = Time.now
+  while condition.call && ((Time.now - t0) * 1000 < timeout)
+    sleep 0.1
+  end
+end
+
 RSpec.describe 'resque-heroku-signals' do
 
   context "Signal handling" do
@@ -15,7 +41,15 @@ RSpec.describe 'resque-heroku-signals' do
       @uuid = SecureRandom.uuid
       ENV["TERM_CHILD"] = "1"
 
+      # match production timeouts, this ensures that there are not any edge
+      # cases in the underlying resque logic that may conflict with our patches
+      ENV["RESQUE_PRE_SHUTDOWN_TIMEOUT"] = "20"
+      ENV["RESQUE_TERM_TIMEOUT"] = "8"
+
       @worker = Resque::Worker.new(:jobs)
+
+      # by default, resque doesn't log anything
+      @worker.very_verbose = true
     end
 
     after do
@@ -38,35 +72,46 @@ RSpec.describe 'resque-heroku-signals' do
     end
 
     it "ignores the first TERM signal but raises an exception on the second signal" do
-      thread = Thread.new do
-        @worker.work(1)
-      end
+      thread = Thread.new { @worker.work(0.1) { puts "hello" } }
+
       Resque::Job.create(:jobs, DummyJob, @uuid)
       pid = get_job_pid(@worker)
-      Process.kill("TERM", pid)
-      sleep 0 # It seems like the second signal isn't received without this
-      Process.kill("TERM", pid)
+
+      expect(pid).to_not be_nil
+
+      Process.kill(:TERM, pid)
+
+
+      # It seems like the second signal isn't received without this
+      sleep 0.1
+      Process.kill(:TERM, pid)
 
       @worker.shutdown
       thread.join
 
+      # Implied assertion here is if the file does not exist, then an exception was raised
       expect(File.exist?(@uuid)).to eq(false)
     end
-  end
-end
 
-def get_job_pid(worker)
-  pid = nil
-  wait condition: -> () {
-    pid = worker.instance_variable_get("@child")
-    pid.nil?
-  }
-  pid
-end
+    it 'provides a flag indicating if heroku will soon terminate the worker' do
+      expect(Resque.heroku_will_terminate?).to be false
 
-def wait(timeout: 5000, condition:)
-  t0 = Time.now
-  while condition.call && ((Time.now - t0) * 1000 < timeout)
-    sleep 0.1
+      # `work` must be run in a separate thread, a new process is only created
+      # after a job is picked up. The current thread is used to run the job loop
+      thread = Thread.new { @worker.work(0.1) { puts "hello" } }
+
+      Resque::Job.create(:jobs, LongCleanupJob, @uuid)
+      pid = get_job_pid(@worker)
+
+      expect(pid).to_not be_nil
+
+      Process.kill(:TERM, pid)
+
+      @worker.shutdown
+      thread.join
+
+      # if worker will terminate, then the file is written
+      expect(File.exist?(@uuid)).to eq(true)
+    end
   end
 end
