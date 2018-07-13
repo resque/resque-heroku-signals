33e05ec2700ce5d3295b873acc636f83ed127ddd
Michael Bianco <iloveitaly@gmail.com>
2018-07-13 07:48:11 -0600

    Adding heroku_will_terminate? to determine if a TERM signal has been sent

--- lib/resque-heroku-signals.rb
+++ lib/resque-heroku-signals.rb
@@ -1,9 +1,19 @@
 require 'resque'
 
+$HEROKU_WILL_TERMINATE_RESQUE = false
+
+Resque.class_eval do
+  def self.heroku_will_terminate?
+    !!$HEROKU_WILL_TERMINATE_RESQUE
+  end
+end
+
 # https://github.com/resque/resque/issues/1559#issuecomment-310908574
 Resque::Worker.class_eval do
   def unregister_signal_handlers
     trap('TERM') do
+      $HEROKU_WILL_TERMINATE_RESQUE = true
+
       trap('TERM') do
         log_with_severity :info, "[resque-heroku] received second term signal, throwing term exception"
 
@@ -15,7 +25,6 @@ Resque::Worker.class_eval do
       end
 
       log_with_severity :info, "[resque-heroku] received first term signal from heroku, ignoring"
-
     end
 
     trap('INT', 'DEFAULT')
